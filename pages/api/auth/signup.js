0b7543dd46eebbde701e5760d940ac09b685e0f0
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-02-14 12:09:43 +0000

    Add front end request

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,6 +2,10 @@ import { connnectToDatabase } from '../../../lib/db'
 import { hashPassword } from '../../../lib/auth'
 
 async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return
+  }
+
   const data = req.body
   const { email, password } = data
 
@@ -12,22 +16,27 @@ async function handler(req, res) {
     password.trim().length < 7
   ) {
     res.status(422).json({
-      message: 'Invalid input - password should also be at leat 7 characters',
+      message: 'Invalid input - password should also be at least 7 characters',
     })
   }
+  try {
+    const client = await connnectToDatabase()
 
-  const client = await connnectToDatabase()
+    const db = client.db()
 
-  const db = client.db()
+    const hashedPassword = await hashPassword(password)
 
-  const hashedPassword = await hashPassword(password)
+    const result = await db.collection('users').insertOne({
+      email: email,
+      password: hashedPassword,
+    })
 
-  const result = await db.collection('users').insertOne({
-    email: email,
-    password: hashedPassword,
-  })
-
-  res.status(201).json({ message: 'Created user!' })
+    res.status(201).json({ message: 'Created user!' })
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: error.message + 'Failed to Connect to database' })
+  }
 }
 
 export default handler
