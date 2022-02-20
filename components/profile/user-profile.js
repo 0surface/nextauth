effad812ebadfb3fc4f9e4882f9eec9242d70315
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-02-20 15:52:59 +0000

    remove client guard, not needed in this scenario

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,23 +1,7 @@
-import { getSession } from 'next-auth/client'
-import { useState, useEffect } from 'react'
 import ProfileForm from './profile-form'
 import classes from './user-profile.module.css'
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    const isSesssion = async () => {
-      const session = await getSession()
-      !session ? (window.location.href = '/auth') : setIsLoading(false)
-    }
-    isSesssion()
-  }, [])
-
-  if (isLoading) {
-    return <p className={classes.profile}> Loading...</p>
-  }
-
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
